ccbf9d45ca65051b0989adec91d5c675daaca263
SENA <SENA@ARMSCCCCTGOD048.FormacionSena.RED>
2024-08-23 07:45:10 -0500

    Punto 2

--- PasarInformacionGUI/src/main/java/jcd/pasarinformaciongui/ventanaPrincipal.java
+++ PasarInformacionGUI/src/main/java/jcd/pasarinformaciongui/ventanaPrincipal.java
@@ -24,28 +24,37 @@ public class ventanaPrincipal extends JFrame implements ActionListener {
         btnTraspasa1 = new Button("Traspasa 1");
         btnTraspasa2 = new Button("Traspasa 2");
  
-        etiTexto1.setText("A");
-        etiTexto2.setText("A");
-        
         btnTraspasa1.addActionListener(this);
         btnTraspasa2.addActionListener(this);
         
         txtTexto.setBounds(16, 16, 252, 24);
+        btnTraspasa1.setBounds(16, 48, 100, 24);
+        btnTraspasa2.setBounds(16, 88, 100, 24);
+        etiTexto1.setBounds(124, 48, 200, 24);
+        etiTexto2.setBounds(124, 88, 200, 24);
         
         mainPanel.setBackground(Color.getHSBColor(120, 50, 250));
         mainPanel.add(txtTexto);
+        mainPanel.add(btnTraspasa1);
+        mainPanel.add(btnTraspasa2);
+        mainPanel.add(etiTexto1);
+        mainPanel.add(etiTexto2);
         mainPanel.setLayout(null);
         add(mainPanel);
 
         setTitle("Mostrar cosas");
-        setSize(300, 120);
+        setSize(300, 164);
         setLocationRelativeTo(null);
         setResizable(false);
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == btnTraspasa1) {
+            etiTexto1.setText(txtTexto.getText());
+        } else if (e.getSource() == btnTraspasa2) {
+            etiTexto2.setText(txtTexto.getText());
+        }
     }
     
 }
